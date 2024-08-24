edeb6acaae55db415efb1b14f0225fa6a7f27965
David Badiei <davidbadiei123@gmail.com>
2024-08-24 12:35:24 -0400

    add toolbar

--- QPDFViewer/Viewer.cpp
+++ QPDFViewer/Viewer.cpp
@@ -1,5 +1,13 @@
-#include "Viewer.h"
+﻿#include "Viewer.h"
 #include <qmenubar.h>
+#include <qapplication.h>
+#include <qmessagebox.h>
+#include <qtoolbar.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qfiledialog.h>
+#include <mupdf/fitz.h>
 
 Viewer::Viewer(QWidget* parent)
 {
@@ -17,12 +25,52 @@ Viewer::Viewer(QWidget* parent)
 	QAction* openAct = new QAction(tr("&Open..."), this);
 	connect(openAct, &QAction::triggered, this, &Viewer::openFile);
 	fileMenu->addAction(openAct);
+	QAction* printAct = new QAction(tr("&Print"), this);
+	fileMenu->addAction(printAct);
+	fileMenu->addSeparator();
 	QAction* exitAct = new QAction(tr("&Exit"), this);
 	fileMenu->addAction(exitAct);
 	connect(exitAct, &QAction::triggered, this, &Viewer::exitApp);
 	QAction* aboutAct = new QAction(tr("&About"), this);
 	aboutmenu->addAction(aboutAct);
 	connect(aboutAct, &QAction::triggered, this, &Viewer::aboutApp);
+
+	//Add toolbar
+	QToolBar* toolBar = new QToolBar(this);
+	this->addToolBar(toolBar);
+	toolBar->setMovable(false);
+	toolBar->setFixedHeight(30);
+	QLabel* pageLbl = new QLabel(this);
+	pageLbl->setText("Page: ");
+	toolBar->addWidget(pageLbl);
+	QLineEdit* pageNumber = new QLineEdit(this);
+	pageNumber->setMaximumWidth(50);
+	pageNumber->setFixedHeight(20);
+	pageNumber->setAlignment(Qt::AlignCenter);
+	toolBar->addWidget(pageNumber);
+	QPushButton* upButton = new QPushButton(this);
+	upButton->setText(QString::fromUtf8(u8"▲"));
+	upButton->setFixedWidth(45);
+	toolBar->addWidget(upButton);
+	QPushButton* downButton = new QPushButton(this);
+	downButton->setText(QString::fromUtf8(u8"▼"));
+	downButton->setFixedWidth(45);
+	toolBar->addWidget(downButton);
+	QLabel* totalPage = new QLabel(this);
+	totalPage->setText(" of ");
+	toolBar->addWidget(totalPage);
+	QLabel* scaleLbl = new QLabel(this);
+	scaleLbl->setText("Scale Document:");
+	toolBar->addWidget(scaleLbl);
+	QLineEdit* searchBox = new QLineEdit(this);
+	searchBox->setFixedHeight(20);
+	toolBar->addWidget(searchBox);
+	QPushButton* backwardsSearch = new QPushButton(this);
+	backwardsSearch->setText("Look Backwards");
+	toolBar->addWidget(backwardsSearch);
+	QPushButton* forwardsSearch = new QPushButton(this);
+	forwardsSearch->setText("Look Forwards");
+	toolBar->addWidget(forwardsSearch);
 }
 
 Viewer::~Viewer()
@@ -31,15 +79,24 @@ Viewer::~Viewer()
 
 void Viewer::openFile()
 {
+	QString fileName = QFileDialog::getOpenFileName(this,
+		tr("Open PDF file"), NULL, tr("PDF Files (*.pdf)"));
 
+	if (fileName != NULL) {
+		fz_context* ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+	}
 }
 
 void Viewer::exitApp()
 {
+	QApplication::exit();
 }
 
 void Viewer::aboutApp()
 {
+	//Display about box
+	QMessageBox::about(this, tr("About QPDFViewer"),
+		tr("<b>QPDFViewer 1.0</b><br>Written by David Badiei, 2024\nLicensed under GNU General Public License v3 (GPL-3)"));
 }
 
 
